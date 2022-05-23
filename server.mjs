c44316e3930582a98301f2ae77f7743272fbc2b1
SergeyBirukov <birukovsergej121@gmail.com>
2022-05-23 13:26:22 +0500

    3

--- server.mjs
+++ server.mjs
@@ -10,6 +10,17 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+https.createServer({
+  key: fs.readFileSync("certs/server.key"),
+  cert: fs.readFileSync("certs/server.cert"),
+}, app).listen(3001, function () {
+  console.log(
+      "App listening on port 3001!"
+  );
+});
+
+app.use('/static', express.static('spa/build'));
+
 app.get("/client.mjs", (_, res) => {
   res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
   res.sendFile(path.join(rootDir, "client.mjs"), {
